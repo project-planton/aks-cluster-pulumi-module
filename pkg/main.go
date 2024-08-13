f186442d836beb366d482bdcfb8e5cbe401977cb
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-13 15:19:05 +0530

    refactored for protos in one package per api resource

--- pkg/main.go
+++ pkg/main.go
@@ -2,13 +2,13 @@ package pkg
 
 import (
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster/model"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/azure/pulumiazureprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type ResourceStack struct {
-	Input  *model.AksClusterStackInput
+	Input  *akscluster.AksClusterStackInput
 	Labels map[string]string
 }
 
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,11 @@
 package outputs
 
 import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster/model"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
 func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
-	input *model.AksClusterStackInput) *model.AksClusterStackOutputs {
-	return &model.AksClusterStackOutputs{}
+	input *akscluster.AksClusterStackInput) *akscluster.AksClusterStackOutputs {
+	return &akscluster.AksClusterStackOutputs{}
 }
