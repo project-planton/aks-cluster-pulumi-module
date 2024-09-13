f83f68d919f92df6eb11f8f4b1e4f2d897826d9f
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 03:06:10 +0530

    converted to pulumi git repo

--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,5 @@
 package outputs
 
-import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+const (
+	OutputKey = "value"
 )
-
-func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
-	input *akscluster.AksClusterStackInput) *akscluster.AksClusterStackOutputs {
-	return &akscluster.AksClusterStackOutputs{}
-}
