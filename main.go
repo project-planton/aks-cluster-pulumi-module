accaddd7c51ec90e25bf4a41808686f24ba94ab5
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:00:39 +0530

    fixed all compile errors after seperating project-planton and planton-cloud apis

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	aksclusterv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/azure/akscluster/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/aks-cluster-pulumi-module/pkg"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		stackInput := &akscluster.AksClusterStackInput{}
+		stackInput := &aksclusterv1.AksClusterStackInput{}
 
 		if err := stackinput.LoadStackInput(ctx, stackInput); err != nil {
 			return errors.Wrap(err, "failed to load stack-input")
--- pkg/main.go
+++ pkg/main.go
@@ -1,13 +1,13 @@
 package pkg
 
 import (
+	aksclusterv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/azure/akscluster/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/akscluster"
 	"github.com/pulumi/pulumi-azure/sdk/v5/go/azure"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Resources(ctx *pulumi.Context, stackInput *akscluster.AksClusterStackInput) error {
+func Resources(ctx *pulumi.Context, stackInput *aksclusterv1.AksClusterStackInput) error {
 	azureCredential := stackInput.AzureCredential
 	//create azure provider using the credentials from the input
 	_, err := azure.NewProvider(ctx,
